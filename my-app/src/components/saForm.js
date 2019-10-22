fb24de925581a140e9bbf77fc4a952a73af99be3
Alex Vega <alexvega880@Alexs-MacBook-Air.local>
2019-10-21 19:41:58 -0700

    setting up react

---
+++ my-app/src/components/saForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function saForm(props) {
+    return (
+        console.log()
+        )
+}
+
+export default saForm;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import saForm from "./components/saForm";
+import saList from "./components/saList";
+import saItems from "./components/saItems";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -10,3 +29,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
